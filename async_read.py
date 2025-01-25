a9838b9b36a1a51954ee134b27142cede4a68473
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-01-25 16:34:47 -0300

    generalize async_generator for any function

--- async_read.py
+++ async_read.py
@@ -2,21 +2,26 @@
 
 from threading import Thread
 
-def async_lines(path):
+def async_generator(callback):
     return_value = []
 
-    def callback():
-        print('reading file', path)
-        with open(path) as f: return_value.append([*f])
-        print('file read', path)
-
-    thread = Thread(target=callback)
+    thread = Thread(target=lambda: return_value.append(callback()))
     thread.start()
 
     return (
         thread.join() or return_value[0]
         for _ in [None]
     )
+
+def async_lines(path):
+    def read_lines():
+        print('reading file', path)
+        with open(path) as f: return_value = [*f]
+        print('file read', path)
+
+        return return_value
+
+    return async_generator(read_lines)
 
 async_lines1 = async_lines('long1.txt')
 async_lines2 = async_lines('long2.txt')
